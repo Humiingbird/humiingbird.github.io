d1ba8125e0fd788a9116aab87a0ba412c40c8339
ADOLF-O <adolfoleoinfo@gmail.com>
2023-08-21 10:50:07 -0600

    pimer commit de la pruab del host

---
+++ js/main.js
@@ -0,0 +1,150 @@
+window.addEventListener("load", main);
+
+function main(){
+    let boton1 = document.getElementById("slidem1");
+    let boton2 = document.getElementById("slidem2");
+    let boton3 = document.getElementById("slidem3");
+    let boton4 = document.getElementById("slidem4");
+
+    let boton5 = document.getElementById("slidem5");
+    let boton6 = document.getElementById("slidem6");
+    let boton7 = document.getElementById("slidem7");
+    let boton8 = document.getElementById("slidem8");
+
+    let boton9 = document.getElementById("slidem9");
+    let boton10 = document.getElementById("slidem10");
+    let boton11 = document.getElementById("slidem11");
+    let boton12 = document.getElementById("slidem12");
+
+    let boton13 = document.getElementById("slidem13");
+    let boton14 = document.getElementById("slidem14");
+    let boton15 = document.getElementById("slidem15");
+    let boton16 = document.getElementById("slidem16");
+
+    boton1.addEventListener("click", color1)
+    boton2.addEventListener("click", color2)
+    boton3.addEventListener("click", color3)
+    boton4.addEventListener("click", color4)
+    boton1.style.background = "#f70";
+
+    boton5.addEventListener("click", color5)
+    boton6.addEventListener("click", color6)
+    boton7.addEventListener("click", color7)
+    boton8.addEventListener("click", color8)
+    boton5.style.background = "#f70";
+
+    boton9.addEventListener("click", color9)
+    boton10.addEventListener("click", color10)
+    boton11.addEventListener("click", color11)
+    boton12.addEventListener("click", color12)
+    boton9.style.background = "#f70";
+
+    boton13.addEventListener("click", color13)
+    boton14.addEventListener("click", color14)
+    boton15.addEventListener("click", color15)
+    boton16.addEventListener("click", color16)
+    boton13.style.background = "#f70";
+    
+    function color1(){
+        boton1.style.background = "#f70";
+        boton2.style.background = "#fff";
+        boton3.style.background = "#fff";
+        boton4.style.background = "#fff";
+    };
+    function color2(){
+        boton1.style.background = "#fff";
+        boton2.style.background = "#f70";
+        boton3.style.background = "#fff";
+        boton4.style.background = "#fff";
+    };
+    function color3(){
+        boton1.style.background = "#fff";
+        boton2.style.background = "#fff";
+        boton3.style.background = "#f70";
+        boton4.style.background = "#fff";
+    };
+    function color4(){
+        boton1.style.background = "#fff";
+        boton2.style.background = "#fff";
+        boton3.style.background = "#fff";
+        boton4.style.background = "#f70";
+    };
+
+
+
+    function color5(){
+        boton5.style.background = "#f70";
+        boton6.style.background = "#fff";
+        boton7.style.background = "#fff";
+        boton8.style.background = "#fff";
+    };
+    function color6(){
+        boton5.style.background = "#fff";
+        boton6.style.background = "#f70";
+        boton7.style.background = "#fff";
+        boton8.style.background = "#fff";
+    };
+    function color7(){
+        boton5.style.background = "#fff";
+        boton6.style.background = "#fff";
+        boton7.style.background = "#f70";
+        boton8.style.background = "#fff";
+    };
+    function color8(){
+        boton5.style.background = "#fff";
+        boton6.style.background = "#fff";
+        boton7.style.background = "#fff";
+        boton8.style.background = "#f70";
+    };
+
+
+    function color9(){
+        boton9.style.background = "#f70";
+        boton10.style.background = "#fff";
+        boton11.style.background = "#fff";
+        boton12.style.background = "#fff";
+    };
+    function color10(){
+        boton9.style.background = "#fff";
+        boton10.style.background = "#f70";
+        boton11.style.background = "#fff";
+        boton12.style.background = "#fff";
+    };
+    function color11(){
+        boton9.style.background = "#fff";
+        boton10.style.background = "#fff";
+        boton11.style.background = "#f70";
+        boton12.style.background = "#fff";
+    };
+    function color12(){
+        boton9.style.background = "#fff";
+        boton10.style.background = "#fff";
+        boton11.style.background = "#fff";
+        boton12.style.background = "#f70";
+    };
+
+    function color13(){
+        boton13.style.background = "#f70";
+        boton14.style.background = "#fff";
+        boton15.style.background = "#fff";
+        boton16.style.background = "#fff";
+    };
+    function color14(){
+        boton13.style.background = "#fff";
+        boton14.style.background = "#f70";
+        boton15.style.background = "#fff";
+        boton16.style.background = "#fff";
+    };
+    function color15(){
+        boton13.style.background = "#fff";
+        boton14.style.background = "#fff";
+        boton15.style.background = "#f70";
+        boton16.style.background = "#fff";
+    };
+    function color16(){
+        boton13.style.background = "#fff";
+        boton14.style.background = "#fff";
+        boton15.style.background = "#fff";
+        boton16.style.background = "#f70";
+    };
+};
